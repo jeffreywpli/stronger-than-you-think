42ac208aacd9785c1ab0692445619b9682e12ef3
Chris Cai <lcai54@wisc.edu>
2024-04-21 21:11:59 -0500

    accuracy running

--- end_model_training/val_size_experiment.py
+++ end_model_training/val_size_experiment.py
@@ -42,8 +42,8 @@ logger = logging.getLogger(__name__)
 
 target_dict = {
     'f1_binary': ['sms', 'census', 'spouse', 'cdr', 'basketball', 'tennis', 'commercial'],
-    'f1_macro': ['claude9', 'chemprot'],
-    'acc': ['semeval', 'agnews', 'imdb', 'trec', 'yelp', 'youtube', 'news-category', 'amazon31', 'banking77', 'massive18', 'dbpedia-219', 'massive_lowcard', 'dbpedia'],
+    'f1_macro': ['claude9'],
+    'acc': ['semeval', 'agnews', 'imdb', 'trec', 'yelp', 'youtube', 'news-category', 'amazon31', 'banking77', 'massive18', 'dbpedia-219', 'massive_lowcard', 'dbpedia', 'chemprot'],
 }
 
 token_dict = {
